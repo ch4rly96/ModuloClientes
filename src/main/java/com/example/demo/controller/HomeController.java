66af0a5c9343f9bb1e5eb08f846ffd69d50bb623
Charly Villanueva <charlyvg240896@gmail.com>
2025-11-17 21:40:20 -0500

    agregado parte de las interfaces falta modificar aun

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
+    @GetMapping
+    public String dashboard(Model model) {
+        model.addAttribute("title", "Dashboard");
 
-    @GetMapping("/")
-    public String home() {
-        return "index";
+        model.addAttribute("content", "home/index :: content");
+
+        return "layout/main";
     }
 }
