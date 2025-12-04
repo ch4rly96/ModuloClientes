a40905b3b2ccc3dff7bd61733e677f7c3c4244ea
Angiemsa25 <U22320718@utp.edu.pe>
2025-12-04 01:46:25 -0500

    reclamosss

--- src/main/java/com/example/demo/controller/ReclamoWebController.java
+++ src/main/java/com/example/demo/controller/ReclamoWebController.java
@@ -90,7 +90,7 @@ public class ReclamoWebController {
             @RequestParam String solucion,
             @RequestParam String usuarioNombre) {
         reclamoService.actualizarEstado(id, estado, solucion, usuarioNombre);
-        return "redirect:/reclamos/" + id;
+        return "redirect:/reclamos";
     }
 
     // ELIMINAR
